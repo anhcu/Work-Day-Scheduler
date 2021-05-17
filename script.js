0df28e7dd8c2b67f5182288802af6777b8c41d51
Hunter <no@no.com>
2021-05-17 18:23:48 -0400

    update date

---
+++ script.js
@@ -0,0 +1,22 @@
+var storedItem = localStorage.getItem('storedItem');
+
+function save(){
+    var item = document.getElementsByTagName('textarea')
+}
+
+// let textContent = document.getElementsByTagNameNS('textarea');
+// let add = document.getElementsByClassName('saveBtn');
+
+
+// console.log('textContent')
+
+// const text = document.querySelector('text');
+// console.log('text')
+
+
+// const button = document.querySelector('saveBtn');
+
+// text.addEventListener('input', letter => {
+//     console.log(letter)
+//     text.textContent = letter.target.value
+// })

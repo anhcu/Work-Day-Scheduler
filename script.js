bf6cb6fb211632aface2c20d6ba91d9d666a3473
Hunter <no@no.com>
2021-05-20 21:53:17 -0400

    save-function-work

--- script.js
+++ script.js
@@ -1,22 +1,90 @@
-var storedItem = localStorage.getItem('storedItem');
+//save & text comment 
+// var saveButton = document.getElementById("save");
+// var comment = document.getElementById("text")
 
-function save(){
-    var item = document.getElementsByTagName('textarea')
+// comment.textContent= localStorage.getItem('comment') 
+// var textArea = localStorage.getItem('comment') 
+// console.log(textArea)
+
+//saveButton.addEventListener("click", () => {
+  //  console.log('work')
+    // console.log('comment',comment.value)
+//localStorage.setItem("comment", comment.value);
+//});
+
+// //date and time (nned to display)
+var date = new Date();
+var currentDay = date.toDateString();
+// var time = date.toLocaleTimeString();
+document.getElementById("currentDay").innerHTML = currentDay
+
+
+//document.ready allows for the methods within the scope of it - to be envoked once the 
+// page has fully loaded on the browser 
+$(document).ready (function(){ 
+// logic 
+//transitioning Vanilla JS logic to Jquery Logic
+//variables that will be used for Jquery logic
+//theTime will equate to the id of the parent ( currnt row)
+
+//TO add event listener to the buttons (all buttons)
+$(".save").click(function() {
+    var theTime =  $(this).parent().attr("id");
+var theContent =  $(this).siblings(".content").val();
+    console.log('working')
+  //grab the value along with the time assciate with the note 
+ localStorage.setItem(theTime, theContent);
+});
+
+
+
+// content.textContent= localStorage.getItem('content') 
+// var textArea = localStorage.getItem('comment') 
+// console.log(textArea)
+
+//saveButton.addEventListener("click", () => {
+  //  console.log('work')
+    // console.log('comment',comment.value)
+//localStorage.setItem("comment", comment.value);
+
+
+
+
+//create a conditional method that will alter the DOM based on the current time
+// if the time has past - add the past class  and vice versa withthe present class
+function timeChanged () {
+    //grab the current time of the user using moment CDN 
+  var theUsersCurrentTime = moment().hours();
+  console.log(theUsersCurrentTime)
+
+  //logic that will impact the hours one by one (using a DRY method)
+  $(".row").each(function(){
+     
+      //grab the id of the rows and the value
+      var theTimeRow = parseInt($(this).attr("id").split("-")[1])
+
+
+      //check the status
+if (theTimeRow < theUsersCurrentTime) {
+    // addClass is method by JQuery 
+    // the past styling will be added to the designated row
+    $(this).addClss("past");
 }
+else if (theTimeRow === theUsersCurrentTime){ 
+    $(this).removeClass("past");
+    $(this).addClass("present");
+}else
+    $(this).removeClass("past");
+    $(this).removeClass("present");
+    $(this).addClass("future");
+  })
+}
+//by placing this within the scope f document.ready() - the timeChanged function 
+//witll automatically execute once the page loads 
+timeChanged()
 
-// let textContent = document.getElementsByTagNameNS('textarea');
-// let add = document.getElementsByClassName('saveBtn');
+})
 
 
-// console.log('textContent')
-
-// const text = document.querySelector('text');
-// console.log('text')
 
 
-// const button = document.querySelector('saveBtn');
-
-// text.addEventListener('input', letter => {
-//     console.log(letter)
-//     text.textContent = letter.target.value
-// })
